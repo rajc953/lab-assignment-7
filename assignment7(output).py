335c56bfc44d02da375dd410bb168b10d3d72982
rajc953 <103478704+rajc953@users.noreply.github.com>
2022-07-16 22:20:56 +0530

    Add files via upload

---
+++ assignment7(output).py
@@ -0,0 +1,27 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:/Users/HP/Dropbox/PC/Downloads/assignment_7.py ==========
+Enter size of list: 4
+Enter marks: 2
+Enter marks: 23
+Enter marks: 12
+Enter marks: 67
+[2, 12, 23, 67]
+Enter size of array: 5
+Enter element 23
+Enter element 45
+Enter element 67
+Enter element 4
+Enter element 89
+[4, 23, 45, 67, 89]
+Enter element to be searched and counted: 4
+Element is present at index 0
+Number of occurrences of element is 1
+Enter number of inputs:5
+Enter number:1
+Enter number:2
+Enter number:3
+Enter number:2
+Enter number:3
+[1, 2, 3]
